34ef953a1d3778604308ae6a6f80542cef8177b0
Robertas Ankudovicius <r.ankudovicius@gmail.com>
2025-05-19 18:56:51 +0300

    Added fetch

--- components/Main/Main.jsx
+++ components/Main/Main.jsx
@@ -1,52 +1,45 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 import styles from "./styles.module.css";
 import Article from "../Article/Article";
+import Spinner from "../Spinner/Spinner";
 
 const Main = () => {
-  const [articles, setArticles] = useState([
-    {
-      id: "1",
-      title: "Goldis",
-      imgUrl:
-        "https://images.theconversation.com/files/625049/original/file-20241010-15-95v3ha.jpg?ixlib=rb-4.1.0&rect=12%2C96%2C2671%2C1335&q=45&auto=format&w=1356&h=668&fit=crop",
-    },
-    {
-      id: "2",
-      title: "Pug",
-      imgUrl:
-        "https://www.dogstrust.org.uk/images/800x600/assets/2025-03/toffee%202.jpg",
-    },
-    {
-      id: "3",
-      title: "Cihuahua",
-      imgUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB3AZLYQbah51ZWbFob7YokxP-ZdE-WV8Us4mSrSbNPA&s&ec=72940543",
-    },
-    {
-      id: "4",
-      title: "Dobermanas",
-      imgUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB3AZLYQbah51ZWbFob7YokxP-ZdE-WV8Us4mSrSbNPA&s&ec=72940543",
-    },
-    {
-      id: "5",
-      title: "Corgis",
-      imgUrl:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRB3AZLYQbah51ZWbFob7YokxP-ZdE-WV8Us4mSrSbNPA&s&ec=72940543",
-    },
-  ]);
+  const [candies, setCandies] = useState(null);
+
+  const fetchCandies = async () => {
+    const response = await axios.get(
+      "https://680726a0e81df7060eb8fba8.mockapi.io/candies"
+    );
+
+    setCandies(response.data);
+  };
+
+  useEffect(async () => {
+    await axios.get("https://680726a0e81df7060eb8fba8.mockapi.io/candies");
+
+    fetchCandies();
+  }, []);
+
+  console.log(candies);
 
   return (
     <main className={styles.wrapper}>
-      {articles.map((article) => {
-        return (
-          <Article
-            key={article.id}
-            title={article.title}
-            imgUrl={article.imgUrl}
-          />
-        );
-      })}
+      {candies ? (
+        <>
+          {candies.map((candy) => {
+            return (
+              <Article
+                key={candy.id}
+                title={candy.title}
+                imgUrl={candy.imgUrl}
+              />
+            );
+          })}
+        </>
+      ) : (
+        <Spinner />
+      )}
     </main>
   );
 };
